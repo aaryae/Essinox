ce4dc25c1bd461fdc53413568851855f9565fb96
ankitnep001 <ankit001nep@gmail.com>
2026-01-04 08:42:51 +0545

    chore: adding new images

--- src/config/constant/image.js
+++ src/config/constant/image.js
@@ -1,6 +1,9 @@
 
-import glass from "@assets/image/landing/glass.png"
+import designerCollection from "@assets/image/landing/designerCollection.png"
+import designerCollection2 from "@assets/image/landing/designerCollection2.png"
+import designerCollection3 from "@assets/image/landing/designerCollection3.png"
 import essinoxGlass from "@assets/image/landing/essinoxGlass.png"
+import glass from "@assets/image/landing/glass.png"
 
 import groupLogo1 from "@assets/image/logo/groupLogo1.png"
 import groupLogo2 from "@assets/image/logo/groupLogo2.png"
@@ -14,6 +17,9 @@ export const image = {
     groupLogo2,
     heroVideo: heroVideoFile,
     glass,
-    essinoxGlass
+    essinoxGlass,
+    designerCollection,
+    designerCollection2,
+    designerCollection3
 
 }   
